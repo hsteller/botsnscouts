31fea1d04ce06fe6c46e40f65b97004128f43014
hendrik <>
2000-03-15 23:21:44 +0000

    Eine Klasse, die statistisches Zeug ueber die Roboter speichern wird.

---
+++ bns/de/botsnscouts/util/Stats.java
@@ -0,0 +1,79 @@
+package de.botsnscouts.util; 
+
+public class Stats {
+    private String name;
+    private int hits;
+    private int kills;
+    private int damageByBoard;
+    private int damageByRobots;
+    
+
+    public Stats (String n) {
+	this.name  = n;
+	this.hits  = 0;
+	this.kills = 0;
+	this.damageByBoard=0;
+	this.damageByRobots=0;
+    }
+
+
+
+    public String getName (){
+	return name;
+    }
+
+    public int getHits(){
+	return hits;
+    }
+    public int getDamageByBoard(){
+	return damageByBoard;
+    }
+    public int getDamageByRobots(){
+	return damageByRobots;
+    }
+    public int getKills(){
+	return kills;
+    }
+    public void setName(String n) {
+	this.name=n;
+    }
+    public void setHits(int i) {
+	this.hits=i;
+    }
+    public void setKills(int i) {
+	this.kills=i;
+    }
+    public void setDamageByBoard(int i) {
+	this.damageByBoard=i;
+    }
+    public void setDamageByRobots(int i) {
+	this.damageByRobots=i;
+    }
+
+    public String toSendString () {
+	return (this.name+","+this.hits+","+this.kills+","+this.damageByBoard+","+this.damageByRobots);
+    }
+
+    public String toString () {
+	return ("Robot: "+name+"\tHits: "+hits+"\tKills: "+kills+"\tdamByBoard:"+damageByBoard+"\tdamByRobot:"+this.damageByRobots);
+    }
+    public boolean less (Stats s) {
+	if (this.hits<s.getHits())
+	    return true;
+	else if (this.hits==s.getHits()) {
+	    if (this.kills<s.getKills())
+		return true;
+	    else if (this.kills==s.kills){
+		if (this.name.toUpperCase().compareTo(s.name.toUpperCase())<0)
+		    return true;
+		else 
+		    return false;
+	    }
+	    else 
+		return false;
+	}
+	else
+	    return false;
+    }
+
+}
