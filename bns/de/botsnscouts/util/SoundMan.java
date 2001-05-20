cc5fa80d4397805d1c3a736b82d16ade29dfdbe7
miriam <>
2001-05-20 00:07:25 +0000

    Small refactoring in SoundMan to make adding sounds easier. Resource saving: Load sounds only if sound is enabled. Plus: Added a new sound.

--- bns/de/botsnscouts/util/SoundMan.java
+++ bns/de/botsnscouts/util/SoundMan.java
@@ -6,79 +6,103 @@ import de.botsnscouts.BotsNScouts;
 
 import org.apache.log4j.Category;
 
-
+/** SoundMan configures, loads and plays the sounds
+ *  To add a new sound, simply add a constant and add the filename
+ *  to the array at the appropiate position.
+ *  If you add a laser sound, add the constant to the laser sound array.
+ */
 public class SoundMan {
   static Category CAT = Category.getInstance(SoundMan.class);
 
+    /** Sound of robot falling in a pit. */
+    public static final int PIT = 0;
+    /** Sound of robot laser */
+    public static final int LASER1 = 1;
+    /** Another Sound of robot laser */
+    public static final int LASER2 = 2;
+    /** Sound of robot reaching a flag. */
+    public static final int FLAG_REACHED = 3;
 
-  private static int actualLaserSound=0;
-  private static int numberOfLaserSounds=2;
+    /** How to locate the sounds relative to de.botsnscouts.BotsNScouts 
+     *  Mind the ordering.
+     */
+    private static final String[] filenames = {
+	"sounds/pit.wav",
+	"sounds/laserhit.wav",
+	"sounds/laser2.wav",
+	"sounds/flag_reached.wav"
+    };
 
-  private static AudioClip [] laserSounds = new AudioClip[numberOfLaserSounds];
-  private static AudioClip pitSound;
+    /** These sounds are robot laser sounds */
+    private static final int[] laserSounds = {LASER1, LASER2};
 
-  private static boolean soundsLoaded;
-  private static boolean soundsActive;
+    private static final AudioClip[] sounds = new AudioClip[filenames.length];
+
+    private static int actualLaserSound=0;
+
+    private static boolean soundsLoaded;
+    private static boolean soundsEnabled;
 
   public SoundMan() {
     actualLaserSound=0;
   }
 
+    public static synchronized void playSound(int sound){
+	try {
+	    if (soundsLoaded && soundsEnabled){
+		sounds[sound].play();
+	    } 
+	}catch (ArrayIndexOutOfBoundsException ex){
+	    CAT.error("Invalid sound constant chosen.");
+	}
+    } 
+
   public static synchronized void playNextLaserSound(){
-    if (soundsLoaded&&soundsActive){
-      laserSounds[actualLaserSound].play();
-      actualLaserSound = ++actualLaserSound%numberOfLaserSounds;
-    }
+      try {
+	  playSound(laserSounds[actualLaserSound]);
+	  actualLaserSound = ++actualLaserSound%laserSounds.length;
+      } catch(ArrayIndexOutOfBoundsException ex){
+	  CAT.error("Invalid laser sound constant chosen.");
+      }
   }
 
   public static synchronized void playPitFallSound() {
-    if (soundsLoaded&&soundsActive)
-      pitSound.play();
+      playSound(PIT);
   }
 
   // does it need to be synchronized ..??
   // does it hurt..?? Hmm, guess no ;-)
   public static synchronized void setSoundActive(boolean soundOn){
-    soundsActive=soundOn;
+    soundsEnabled=soundOn;
+    if (!soundsLoaded)
+	loadSounds();
   }
 
   public synchronized static boolean isSoundActive(){
-    return soundsActive&&soundsLoaded;
+    return soundsEnabled&&soundsLoaded;
   }
 
 
   public static synchronized void loadSounds() {
-	if (soundsLoaded)
+      if (soundsLoaded || (!soundsEnabled))
 	    return;
 	Thread t = new BNSThread () {
 		public void run() {
                   try {
 
 		    SoundMan.CAT.debug("Initializing sounds..");
-	            soundsLoaded=false;
 
-                    pitSound = loadSound("sounds/pit.wav");
-		    laserSounds [0] = loadSound("sounds/laserhit.wav");
-		    laserSounds [1] = loadSound("sounds/laser2.wav");
+		    for (int i=0; i<sounds.length; i++){
+			sounds[i] = loadSound(filenames[i]);
+			if (sounds[i] == null)
+			    throw new Exception("Sound "+filenames[i]+" was not loaded");
+		    }
+		    
+		    soundsLoaded = true;
 
-                    boolean error = false;
-		    if (laserSounds [0] == null) {
-			SoundMan.CAT.warn("laserhit.wav not located :-(");
-			error=true;
-		    }
-		    if (laserSounds [1] == null){
-			SoundMan.CAT.warn("laser2.wav not located :-(");
-			error = true;
-		    }
-		    if (error)
-			SoundMan.CAT.error("Failed to load sounds; sounds deactivated");
-		    else{
-			SoundMan.CAT.debug("Sounds loaded!");
-			soundsLoaded=true;
-		    }
-                 }
-                 catch (Throwable t) {
-                    SoundMan.CAT.error("Error occured while loading sounds");
+		  } catch (Throwable t) {
+		    SoundMan.CAT.error("Error occured while loading sounds");
+		    SoundMan.CAT.debug(t);
                     SoundMan.CAT.error("sounds deactivated for safety..");
                     soundsLoaded=false;
                  }
