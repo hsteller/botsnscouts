d2f068ef4a964d1efb0b63f4b4d5c8a0004899ed
materlik <>
2001-06-02 02:57:59 +0000

    hopefully doing its thing under win$

---
+++ bns/tools/distrib.binary/tileeditor.bat
@@ -0,0 +1,1 @@
+java -cp botsnscouts.jar de.botsnscouts.editor.BoardEditor
---
+++ bns/tools/distrib.src/start.bat
@@ -0,0 +1,1 @@
+java -cp classes;lib\sixlegs.jar;lib\log4j.jar;lib\jakarta-regexp-1.2.jar;lib\pngenc.jar de.botsnscouts.BotsNScouts
