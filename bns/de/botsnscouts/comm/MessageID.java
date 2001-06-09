a3a28ed49fba6e59983a4d07dbfca381cae23d88
henni <>
2001-06-09 02:30:08 +0000

    *** empty log message ***

---
+++ bns/de/botsnscouts/comm/MessageID.java
@@ -0,0 +1,15 @@
+package de.botsnscouts.comm;
+
+public interface MessageID {
+
+  public static final String BOT_CRUSHED     = "mBotCrushed";
+  public static final String BOT_IN_PIT      = "mGrubenopfer";
+  public static final String BOT_LASER       = "mRobLaser";
+  public static final String WISE_USED       = "mKlugSchKlick";
+  public static final String BORD_LASER_SHOT = "mBoardLaser";
+  public static final String FLAG_REACHED    = "mNextFlag";
+
+  public static final String AUSWERTUNG      = "mAusw";
+
+
+}
