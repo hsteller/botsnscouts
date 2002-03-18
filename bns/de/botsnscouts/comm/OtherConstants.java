9af6123d1dfec1a8c05a21e760c52e1057499243
henni <>
2002-03-18 01:17:35 +0000

    some communication constants I did not know where to put ;)

---
+++ bns/de/botsnscouts/comm/OtherConstants.java
@@ -0,0 +1,10 @@
+package de.botsnscouts.comm;
+
+public interface OtherConstants {
+
+   // public static final String REASON_QUIT           = "GONE";
+    public static final String REASON_LOST_LIVES     = "LL";
+    public static final String REASON_TIMEOUT        = "TO";
+    public static final String REASON_RULE_VIOLATION = "RV";
+
+}
