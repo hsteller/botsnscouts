d0a30d6f0c8d65739c1b31d8d1188191f76b9db4
henni <>
2004-07-21 21:54:30 +0000

    fixed the messed up GridbagLayout of the "start autobox" box: * reordered the widgets in the box * textfield for the name is visible again * intelligence slider looks good  again (broader)

--- bns/de/botsnscouts/start/StartPanel.java
+++ bns/de/botsnscouts/start/StartPanel.java
@@ -179,10 +179,10 @@ public class StartPanel extends JPanel {
             }
         });
 
-        gc.gridx = 0;
+        gc.gridx = 1;
         gc.gridwidth = 1;
         gc.gridy++;
-        gc.anchor = GridBagConstraints.WEST;
+        gc.anchor = GridBagConstraints.EAST;
         gc.fill = GridBagConstraints.CENTER;
         gc.insets = new Insets(0, 0, 0, 0);
         panel.add(ok, gc);
@@ -201,27 +201,57 @@ public class StartPanel extends JPanel {
         GridBagConstraints gc = new GridBagConstraints();
         Insets noInsets = new Insets(0, 0, 0, 0);
         Insets insets = new Insets(0, 0, 20, 0);
-
-        JLabel label = new TJLabel(Message.say("Start", "mStartKS"));
-        gc.gridwidth = GridBagConstraints.REMAINDER;
-        gc.gridy = 1;
+        
+        // headline for the whole box
+        JLabel label = new TJLabel(Message.say("Start", "mStartKS"), JLabel.CENTER);
+        gc.weightx = 1.0;
+        gc.gridx = 0;
+        gc.gridwidth = 4;
+        gc.gridy = 0;
         gc.insets = insets;
         gc.ipadx = 5;
         gc.ipady = 5;
+        gc.anchor = GridBagConstraints.CENTER;
+        gc.fill = GridBagConstraints.HORIZONTAL;
         label.setFont(font);
         panel.add(label, gc);
 
-        JLabel ks = new TJLabel(Message.say("Start", "mIntel"));
-        gc.gridy++;
+        
+        JLabel nameLabel = new JLabel(Message.say("Start", "mKSName"), JLabel.LEFT);
+        gc.weightx = 0.0;
+        gc.gridy++; // 3
+        gc.gridx = 0;
         gc.gridwidth = 1;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.fill = GridBagConstraints.NONE;
+        gc.insets = new Insets(0,0,10,5);
+        panel.add(nameLabel, gc);
+        
+        final JTextField botNameField = new JTextField(KrimsKrams.randomName());
+        botNameField.setColumns(20);
+        gc.gridx = 1;
+        gc.gridwidth = 2;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(botNameField, gc);
+        
+        // headline above the intelligence slider
+        JLabel ks = new TJLabel(Message.say("Start", "mIntel"), JLabel.CENTER);
+        gc.gridy++;        
+        gc.gridx = 1;
+        gc.gridwidth = 2;
+        gc.weightx = 1.0;
+        gc.insets = noInsets;     
         gc.anchor = GridBagConstraints.CENTER;
-        gc.insets = noInsets;
+        gc.fill = GridBagConstraints.HORIZONTAL;      
         panel.add(ks, gc);
 
+        // left label on the slider (intelligent)
         JLabel lb = new TJLabel(Message.say("Start", "mSchlau"), JLabel.LEFT);
-        gc.insets = insets;
-        gc.gridx = GridBagConstraints.RELATIVE;
-        gc.gridy++;
+        gc.gridy++; 
+        gc.gridx = 0;
+        gc.weightx = 0.0;
+        gc.insets = insets;                
         gc.gridwidth = 1;
         gc.gridheight = 1;
         gc.fill = GridBagConstraints.NONE;
@@ -233,39 +263,39 @@ public class StartPanel extends JPanel {
         intel.setOpaque(false);
         intel.getModel().setMinimum(0);
         intel.getModel().setMaximum(130);
+        gc.gridx = 1;
         gc.gridwidth = 2;
         gc.gridheight = 1;
+        gc.weightx = 2.0;
         gc.anchor = GridBagConstraints.CENTER;
-        gc.fill = GridBagConstraints.BOTH;
+        gc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(intel, gc);
 
-        gc.anchor = GridBagConstraints.EAST;
+        // right label on the slider (dumb)
+        gc.weightx = 0.0;
+        gc.gridx = 3;
+        gc.anchor = GridBagConstraints.EAST;       
+        gc.fill = GridBagConstraints.NONE;
         lb = new TJLabel(Message.say("Start", "mDumm"), JLabel.RIGHT);
-        gc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(lb, gc);
-
+        
+        
+        
+       
+        
+        // labeled "likes belts"-checkbox 
         final JCheckBox beltAware = new JCheckBox(Message.say("Start", "beltAware"));
         beltAware.setOpaque(false);
-        beltAware.setToolTipText(Message.say("Start", "beltTooltip"));
+        beltAware.setToolTipText(Message.say("Start", "beltTooltip"));     
         gc.gridy++;
         gc.gridx = 0;
-        gc.gridwidth = 1;
-        gc.anchor = GridBagConstraints.WEST;
+        gc.gridwidth = 2;
+        gc.weightx = 0.0;        
+        gc.anchor = GridBagConstraints.NORTHWEST;
         panel.add(beltAware, gc);
-
-        final JTextField botNameField = new JTextField(KrimsKrams.randomName());
-        botNameField.setColumns(10);
-        JPanel botnamePanel = new JPanel();
-        botnamePanel.setOpaque(false);
-        botnamePanel.add(new JLabel(Message.say("Start", "mKSName")));
-        botnamePanel.add(botNameField);
-        gc.gridx = 2;
-        gc.gridwidth = 1;
-        gc.anchor = GridBagConstraints.EAST;
-        gc.fill = GridBagConstraints.CENTER;
-        gc.insets = noInsets;
-        panel.add(botnamePanel, gc);
-
+        
+        
+        
         JButton startAB = new TransparentButton(Message.say("Start", "mKSStarten"));
         startAB.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -280,15 +310,17 @@ public class StartPanel extends JPanel {
                 botNameField.setText(KrimsKrams.randomName());
             }
         });
-
-        gc.gridx = 4;
-        gc.gridwidth = 1;
+      
+        gc.gridx = 2;
+        gc.gridwidth = 2;
+        gc.gridheight = 2;       
+        gc.weightx = 0.0;              
         gc.anchor = GridBagConstraints.EAST;
-        gc.fill = GridBagConstraints.CENTER;
+        gc.fill = GridBagConstraints.NONE;
         gc.insets = noInsets;
         panel.add(startAB, gc);
         panel.setBorder(new CompoundBorder(new EtchedBorder(8),
-                new EmptyBorder(10, 10, 10, 10)));
+                			new EmptyBorder(10, 10, 10, 10)));
         return panel;
     }
 
