89389d5e57973b5a19d6a32664ac3a4b51aea247
miriam <>
2004-11-05 21:41:47 +0000

    Added string constructor.

--- bns/de/botsnscouts/start/UnableToAnnounceGameException.java
+++ bns/de/botsnscouts/start/UnableToAnnounceGameException.java
@@ -4,4 +4,13 @@ package de.botsnscouts.start;
  * @author Miriam Busch - <miriam.busch@codimi.de>
  */
 public class UnableToAnnounceGameException extends Exception {
+
+    public UnableToAnnounceGameException() {
+        super();
+    }
+
+    public UnableToAnnounceGameException(String mess) {
+        super(mess);
+    }
+
 }
