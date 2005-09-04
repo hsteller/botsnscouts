aaf763d90f5b6897670ee45c6985fd2a24a1c515
henni <>
2005-09-04 03:39:32 +0000

    * more constructors * JDK1.5 troubles: setting caret color to make sure it is visible

--- bns/de/botsnscouts/widgets/TJTextField.java
+++ bns/de/botsnscouts/widgets/TJTextField.java
@@ -1,6 +1,10 @@
 package de.botsnscouts.widgets;
 
+
+
 import javax.swing.JTextField;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.text.Document;
 
 /**
  * @author Miriam Busch - <miriam.busch@codimi.de>
@@ -12,9 +16,33 @@ public class TJTextField extends JTextField {
 
     public TJTextField(String text, int alignment, boolean big) {
         super(text, alignment);
+        init(big);
+    }
+    public TJTextField(int size){
+        super(size);
+        init(false);                
+    }
+    public TJTextField(String text,int size){
+        super(text, size);
+        init(false);                
+    }
+    public TJTextField(){
+        super();
+        init(false);                
+    }
+    
+    public TJTextField(Document doc, String text, int columns){
+        super(doc, text, columns);
+        init(false);
+    }
+    
+    protected void init(boolean useBigFont) {
         setOpaque(false);
         setForeground(GreenTheme.getTextColor());
-        if (big) {
+        setCaretColor(GreenTheme.getBnsCaretColor());
+        setDisabledTextColor(MetalLookAndFeel.getControlDisabled());
+        
+        if (useBigFont) {
             setFont(GreenTheme.getBigFont());
         } else {
             setFont(GreenTheme.getFont());
