06fdeda6fb51342a48d26991b60355e6d5cf1246
henni <>
2005-09-04 03:37:10 +0000

    might be needed

---
+++ bns/de/botsnscouts/widgets/TJComboBox.java
@@ -0,0 +1,40 @@
+/*
+ * Created on 03.09.2005
+ *
+ */
+package de.botsnscouts.widgets;
+
+import java.awt.Color;
+
+import javax.swing.JComboBox;
+import javax.swing.UIManager;
+import javax.swing.plaf.ListUI;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+/**
+ * @author Hendrik Steller
+ * @version $Id$
+ */
+public class TJComboBox extends JComboBox {
+    
+    public TJComboBox(String [] items) {
+        super(items);
+        initTheme();
+    }
+    
+    public TJComboBox() {
+	    super();
+	    initTheme();
+    }
+    
+    
+    
+    private void initTheme() {
+        setOpaque(false);
+        
+        setBackground(GreenTheme.getBnsBackgroundColor());   
+        
+        setForeground(GreenTheme.getTextColor());
+	    setFont(GreenTheme.getFont());
+    }
+}
