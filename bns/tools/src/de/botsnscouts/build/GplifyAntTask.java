2bdb643ebd0f84a8befad1f3995f8124a726dd63
henni <>
2005-10-27 02:28:06 +0000

    should work now; prepare for impact..

--- bns/tools/src/de/botsnscouts/build/GplifyAntTask.java
+++ bns/tools/src/de/botsnscouts/build/GplifyAntTask.java
@@ -1,3 +1,4 @@
+
 /*
  *******************************************************************
  *        Bots 'n' Scouts - Multi-Player networked Java game       *
@@ -22,13 +23,23 @@
  Boston, MA  02111-1307  USA
  
  *******************************************************************/
+
 package de.botsnscouts.build;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Vector;
 
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.BuildException;
 
@@ -42,6 +53,8 @@ import org.apache.tools.ant.BuildException;
  */
 public class GplifyAntTask extends Task {
     
+   /** Only process files with that ending; expected to be lowercase */ 
+    private static final String FILE_EXT_FILTER = ".java";
     
     /** The file that contains the text to be added to the beginning of each .java file (the GPL) */
     private File fileWithTextToInsert;
@@ -74,28 +87,167 @@ public class GplifyAntTask extends Task {
     public void execute() throws BuildException{
         // stuff that could also go into the init-methode; but since execute() will be called
         // only once it doesn't really matter
+        Project bnsproject = getProject();
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
-        BufferedReader in = null;
+        String textToInsert = null;
+    
+       
         try {
-            StringBuffer sb=new StringBuffer();
-            in = new BufferedReader(new FileReader(fileWithTextToInsert));
+            StringBuffer sb = readFileLineWiseIntoBuffer(fileWithTextToInsert);
+            textToInsert = sb.toString().replaceAll(toReplaceWithDate,""+year );
+        }
+        catch (IOException ie){
+            bnsproject.log("error reading the text to insert: "+ie.getMessage(), Project.MSG_ERR);
+            throw new BuildException(ie);
+        }
+        
+        bnsproject.log("adding text: ",Project.MSG_INFO);
+        bnsproject.log(textToInsert, Project.MSG_INFO);
+        
+        File projectRoot = getProject().getBaseDir();
+        FileFilter recursiveOnlyJava = createFilter();
+        Collection filesToCheck = null;
+        try {
+            filesToCheck = collectFiles(projectRoot, recursiveOnlyJava);
+        }
+        catch (IOException ie){
+            bnsproject.log("failed to create the list of files to process; IOException: "+ie.getMessage());
+            throw new BuildException(ie);
+        }
+        
+        boolean haveIgnoreText = ignoreFilesWithThisText != null 
+        									  && !ignoreFilesWithThisText.trim().equals("");
+       
+        // for info messages: 
+        Collection filesSkippedDueToError = new LinkedList();
+        Collection filesIgnored = new LinkedList();
+        try {
+	        for (Iterator fs = filesToCheck.iterator();fs.hasNext(); ){
+	            File f = (File) fs.next();
+	            String content = null;
+	            try { // trying to read the original context of the file:
+	                StringBuffer sb = readFileLineWiseIntoBuffer(f);
+	                content = sb.toString();
+	                int pos = -1;
+	                if (haveIgnoreText) {
+	                    pos = sb.indexOf(ignoreFilesWithThisText);
+	                }
+	                if (pos < 0) { // we were either asked to process all files or the 
+	                    // text that disqualifies files from being processed wasn't found in this file
+	                    content = sb.toString();
+	                }
+	                else {
+	                    filesIgnored.add(f.getAbsolutePath());                    
+	                    content = null;
+	                }
+	            }
+	            catch (IOException ie){
+	                bnsproject.log("IOException during read: "+ie.getMessage(), Project.MSG_ERR);
+	                bnsproject.log("ERROR - SKIPPED: "+f.getAbsolutePath(), Project.MSG_ERR);     
+	                filesSkippedDueToError.add(f.getAbsolutePath());
+	                content = null;
+	            }
+	            if (content != null) { // no error and file is not to be excluded from processing
+	                try {
+	                    bnsproject.log("Processing: "+f.getAbsolutePath()+"..", Project.MSG_INFO);
+	                    PrintWriter out = new PrintWriter(new FileWriter(f, false));
+	                    out.println(textToInsert);
+	                    out.println();
+	                    out.println(content);
+	                    out.close();                    
+	                }
+	                catch (IOException ie){
+	                    bnsproject.log("MAYDAY! I PROBABLY F*CKED UP: "
+	                                    +f.getAbsolutePath()+" !!!", Project.MSG_ERR);
+	                    bnsproject.log("Original content was: ", Project.MSG_ERR);
+	                    bnsproject.log(content, Project.MSG_ERR);
+	                    throw new BuildException("Quitting because of IO-error during file write:"
+	                                    +ie.getMessage());
+	                }
+	               
+	            }
+	           
+	
+	        } // for
+        }
+        finally {
+            int ignSize = filesIgnored.size();
+            if (ignSize>0){
+                for (Iterator it=filesIgnored.iterator();it.hasNext();){
+                    bnsproject.log("ignored: "+it.next(), Project.MSG_INFO);
+                }
+            }
+            int errSize = filesSkippedDueToError.size();
+            if (errSize>0){
+                for (Iterator it=filesSkippedDueToError.iterator();it.hasNext();){
+                    bnsproject.log("SKIPPED (error): "+it.next(), Project.MSG_INFO);
+                }
+            }
+            else {
+                bnsproject.log("no errors", Project.MSG_INFO);
+            }
+        }
+            
+        
+    }
+    
+    /** 
+     * Will create a Collection of File objects:<br>
+     * Starting in directory <code>curDir</code> it will add every file that matches <code>filter</code>.<br>  
+     * Note: to process subdirectories, <code>filter</code> will have to accept directories.<br>
+     * 
+     * @param curDir The directory to start with
+     * @param filter Collect only those files that are accepted by this filter
+     * @return A Collection of File objects for all files that were accepted 
+     * @throws IOException if <code>curDir</code> does not exist 
+     */
+    public static  Collection collectFiles (File curDir, FileFilter filter) throws IOException {    	        
+    	if (curDir == null || !curDir.exists()){    	        	    
+    	    throw new IOException("No such Directory: \""+curDir+"\"");
+    	}    	    	    	
+    	Collection files = new LinkedList();
+    	Vector dirsToDo=new Vector();
+    	dirsToDo.add(curDir);
+    	while (dirsToDo.size()>0) {
+    	    curDir = (File) dirsToDo.remove(0);    	        	    
+    	    File [] all  = curDir.listFiles(filter);   	    
+    	    for (int i=0;i<all.length;i++){
+    	        if (all[i].isDirectory()) {
+    	            dirsToDo.add(all[i]);
+    	        }
+    	        else {    	                	            	               
+	                files.add(all[i]);    	           
+    	        }    	               
+    	    }    		    		    	
+        }
+    	return files;
+    }
+    
+
+    /**
+     *
+     * Reads the content of the text file <code>file</code> into a StringBuffer;
+     * will add a line break after each line (after each call of<code> BufferedReader.readLine()</code>) 
+     * 
+     *  
+     * @param file A text file 
+     * @return The content of the text file in a StringBuffer
+     * @throws IOException if I/O stuff goes wrong while trying to read the file 
+     */
+    public static StringBuffer readFileLineWiseIntoBuffer(File file) throws IOException{
+        BufferedReader in = null;
+        StringBuffer sb=new StringBuffer();
+        try {            
+            in = new BufferedReader(new FileReader(file));
             String s = in.readLine();
             while (s!=null){
                 sb.append(s).append('\n');
                 s = in.readLine();
             }
-            String text = sb.toString().replaceAll(toReplaceWithDate,""+year );
-           
-            // TODO process the files instead..
-            System.out.println(text);
-            
-            
+            return sb;
         }
-        catch (IOException ie){
-            throw new BuildException("IOException while reading the text to insert; message: "+ie.getMessage());
-        }
-        finally {
+        finally { 
             if (in != null) {
                 try {
                     in.close();
@@ -104,8 +256,26 @@ public class GplifyAntTask extends Task {
                 }
             }
         }
-        
     }
     
+    /**
+     * Creates a filter to determine the files to be 'gplified'.
+     * 
+     * @return a filter to determine the files to be 'gplified'
+     */
+    public static FileFilter createFilter() {
+        FileFilter fil = new FileFilter(){                        
+            public boolean accept(File f){
+                return f.isDirectory() || 
+                		  f.getName().toLowerCase().endsWith(FILE_EXT_FILTER);                	    
+            }
+            
+            public String getDescription(){
+                return "Directories and files ending with \""+FILE_EXT_FILTER+"\"";
+            }
+            
+        };
+        return fil;        
+    }
  
 }
